2f35957edc2ed05ff18a443f529f2a13345e6e76
Johnexzy <obajohn75@gmail.com>
2020-06-04 15:28:35 +0100

    removed email

--- script/JohnOba.dart
+++ script/JohnOba.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 void main(List<String> args) {
   _getDay(){
   return jsonEncode({
-    "output": "Hello world, this is Oba John with HNGi7 ID HNG-04363 and email obajohn75@gmail.com using PHP for stage 2 task",
+    "output": "Hello world, this is Oba John with HNGi7 ID HNG-04363  using Dart for stage 2 task",
     "name": "Oba John",
     "id": "HNG-04363",
     "email": "obajohn75@gmail.com",
